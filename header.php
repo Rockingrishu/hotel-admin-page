717415de7d319e0e4694bb4d230e8e88622004c0
unknown <rockingrishu1729@gmail.com>
2023-03-01 03:20:12 +0530

    done some changes

---
+++ header.php
@@ -0,0 +1,33 @@
+<div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
+    <h3 class="mb-0 h-font">HB Webite</h3>
+</div>
+
+
+
+
+<div class="col-lg-2 bg-dark border-top border-3 border-secondry dashboardMenu">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="container-fluid flex-lg-column align-items-stretch">
+            <h4 class="mt-2 text-light">ADMIN PANEL</h4>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse flex-column align-items-stretch mt-2 adminDropdown" id="navbarNavAltMarkup">
+                <<ul class="navbar-nav nav-pills flex-column">
+                    <li class="nav-item ">
+                    <em class="icon ni ni-dashboard-fill"></em>
+                    <a class="nav-link text-white" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Rooms</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="setting.php">Settings</a>
+                    </li>
+                    </ul>
+            </div>
+    </nav>
+</div>
