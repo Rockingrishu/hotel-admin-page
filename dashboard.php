933251ab8205f739f4f764fa9bd52c76df9a3134
unknown <rockingrishu1729@gmail.com>
2023-03-01 00:58:02 +0530

    created a new repo

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Admin Panel -DashBoard</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <?php require("admin/links.php"); ?>
+    </head>
+    <body class="bg-light">
+        <div class="container-fluid bg-dark text-light d-flex align-items-center justify-content">
+        <h3 class="mb-0 h-font">HB Webite</h3>
+        </div>
+        <div class="col-lg-2 bg-warning"></div>
+    </body> 
+</html>
---
+++ links.php
@@ -0,0 +1,3 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
